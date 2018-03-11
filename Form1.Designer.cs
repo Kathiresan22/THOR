2ac5788dc0ea8de48c607e933cecf0296f0c789f
Kathiresan22 <arukathiresan@gmail.com>
2018-03-11 14:54:27 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace thor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imgselectbtn = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.tesspath_textbox = new System.Windows.Forms.TextBox();
+            this.tesspath_btn = new System.Windows.Forms.Button();
+            this.outputPath_textbox = new System.Windows.Forms.TextBox();
+            this.outputpath_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::thor.Properties.Resources.imgnotavail;
+            this.pictureBox1.InitialImage = global::thor.Properties.Resources.imgnotavail;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 360);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imgselectbtn
+            // 
+            this.imgselectbtn.Location = new System.Drawing.Point(86, 457);
+            this.imgselectbtn.Name = "imgselectbtn";
+            this.imgselectbtn.Size = new System.Drawing.Size(137, 23);
+            this.imgselectbtn.TabIndex = 2;
+            this.imgselectbtn.Text = "Select Image";
+            this.imgselectbtn.UseVisualStyleBackColor = true;
+            this.imgselectbtn.Click += new System.EventHandler(this.imgselectbtn_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("ELCOT-Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(394, 74);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(345, 360);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Text = "";
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(494, 457);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(136, 23);
+            this.convert.TabIndex = 4;
+            this.convert.Text = "OCR";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // tesspath_textbox
+            // 
+            this.tesspath_textbox.Location = new System.Drawing.Point(24, 48);
+            this.tesspath_textbox.Name = "tesspath_textbox";
+            this.tesspath_textbox.Size = new System.Drawing.Size(152, 20);
+            this.tesspath_textbox.TabIndex = 5;
+            // 
+            // tesspath_btn
+            // 
+            this.tesspath_btn.Location = new System.Drawing.Point(182, 45);
+            this.tesspath_btn.Name = "tesspath_btn";
+            this.tesspath_btn.Size = new System.Drawing.Size(75, 23);
+            this.tesspath_btn.TabIndex = 6;
+            this.tesspath_btn.Text = "Path";
+            this.tesspath_btn.UseVisualStyleBackColor = true;
+            this.tesspath_btn.Click += new System.EventHandler(this.tesspath_btn_Click);
+            // 
+            // outputPath_textbox
+            // 
+            this.outputPath_textbox.Location = new System.Drawing.Point(394, 47);
+            this.outputPath_textbox.Name = "outputPath_textbox";
+            this.outputPath_textbox.Size = new System.Drawing.Size(164, 20);
+            this.outputPath_textbox.TabIndex = 7;
+            // 
+            // outputpath_btn
+            // 
+            this.outputpath_btn.Location = new System.Drawing.Point(565, 45);
+            this.outputpath_btn.Name = "outputpath_btn";
+            this.outputpath_btn.Size = new System.Drawing.Size(99, 23);
+            this.outputpath_btn.TabIndex = 8;
+            this.outputpath_btn.Text = "Output Path";
+            this.outputpath_btn.UseVisualStyleBackColor = true;
+            this.outputpath_btn.Click += new System.EventHandler(this.outputpath_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 522);
+            this.Controls.Add(this.outputpath_btn);
+            this.Controls.Add(this.outputPath_textbox);
+            this.Controls.Add(this.tesspath_btn);
+            this.Controls.Add(this.tesspath_textbox);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.imgselectbtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button imgselectbtn;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.TextBox tesspath_textbox;
+        private System.Windows.Forms.Button tesspath_btn;
+        private System.Windows.Forms.TextBox outputPath_textbox;
+        private System.Windows.Forms.Button outputpath_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thor
+{
+    public partial class Form1 : Form
+    {
+        String imgpath,tesspath,outpath;
+        public Form1()
+        {
+            InitializeComponent();
+            ChangeLan();
+        }
+       
+
+        private void imgselectbtn_Click(object sender, EventArgs e)
+        {
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box  
+                pictureBox1.Image = new Bitmap(open.FileName);
+                // image file path  
+                //textBox1.Text = open.FileName;
+                imgpath = open.FileName;
+                Console.WriteLine(imgpath);
+            }
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+               
+                // You can start any process, HelloWorld is a do-nothing example.
+                //String outpath = @"E:\THOR\Testing\out";
+                //String tesspath = @"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe";
+                String param=imgpath+" "+outpath+" -l tam";
+                Console.WriteLine(tesspath);
+                Console.WriteLine(param);
+
+
+                Process process = new Process();
+                process.StartInfo.FileName = tesspath;
+                process.StartInfo.Arguments = param;
+                process.StartInfo.WorkingDirectory = @"C:\Program Files (x86)\Tesseract-OCR\";
+                process.Start();
+
+                //Thread.Sleep(3000);
+                readFile();
+                
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine(exe.Message);
+            }
+        }
+
+        public void readFile()
+        {
+            // File.ReadAllText(@"E:\THOR\Testing\out", Encoding.UTF8);
+            // outputTextBox.Text= File.ReadAllText(@"E:\THOR\Testing\out.txt", Encoding.UTF8);
+
+            
+            var fileStream = new FileStream(outpath, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                outputTextBox.Text = streamReader.ReadToEnd();
+            }
+        }
+        private InputLanguage GetTamilLang()
+        {
+            //Enumerate through InstalledInputLanguages which contains 
+            //all the keyboard layout you've installed in your windows. 
+            foreach (InputLanguage lang in InputLanguage.InstalledInputLanguages)
+            {
+                if (lang.LayoutName.ToLower() == "Tamil (India)")
+                    return lang;
+            }
+            return null;
+        }
+
+        public void ChangeLan()
+        {
+            InputLanguage lang = GetTamilLang();
+            if (lang == null)
+                Console.Write("Tamil Language keyboard is not installed.");
+            //Set the current language of the system to 
+            //the InputLanguage instance you need. 
+            InputLanguage.CurrentInputLanguage = lang;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputpath_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                outpath = open.FileName;
+                outputPath_textbox.Text = outpath;
+                Console.WriteLine("Output Path: " + outpath);
+            }
+        }
+
+        private void tesspath_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                tesspath = open.FileName;
+                tesspath_textbox.Text = tesspath;
+                Console.WriteLine("Tessaract Path: " + tesspath);
+            }
+        }
+    }
+}
